f686f235ef14030b2b69919b6acbe7989441419f
Ali Can Yücel <yucelalican@hotmail.com>
2026-08-03 21:34:25 +0300

    Delete CreateTemplateCommand.cs

--- CreateTemplateCommand.cs
+++
@@ -1,7 +0,0 @@
-public record CreateTemplateCommand : IRequest<Result<string>>, IBaseRequest, IEquatable<CreateTemplateCommand>
-{
-    public string Name { get; init; }
-    public string RaporTipi { get; init; }
-    public string ContextHtml { get; init; }
-    public string Content { get; init; } // Added missing property
-}
